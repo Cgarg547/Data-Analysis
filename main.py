e0d9679b399454886c8513fff61b3782dab074be
Cgarg547 <chiraggargkkp@gmail.com>
2024-10-10 01:47:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# importing the dataset using the panda library
+dataset = pd.read_csv("UberDataset.csv")
+dataset.head();

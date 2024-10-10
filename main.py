06e06a0667c86733800231c18f1f7c2ee2800373
Cgarg547 <chiraggargkkp@gmail.com>
2024-10-10 18:23:53 +0000

    data visualisation

--- main.py
+++ main.py
@@ -2,27 +2,49 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime
+
 
 # importing the dataset using the panda library
 dataset = pd.read_csv("UberDataset.csv")
-dataset.head();
 
-# shaping the dataset
-dataset.shape
+#---------------------------------------------
+# print(dataset.head())
+# print(dataset.tail())
 
-# to understand the data more deeply : null values count, datatype, etc
-dataset.info();
+#--------------------------------------------
+# # shaping the dataset
+# print(dataset.shape)
 
-## data processing: replacing the null value with the NOT keyword
-dataset['PURPOSE'].fillna("NOT", inplace=True);
+# --------------------------------------------
+# # to understand the data more deeply : null values count, datatype, etc
+# print(dataset.info())
 
-## changing start_date and end_date to the date_time format
-dataset['START_DATE'] = pd.to_datetime(dataset['START_DATE'], errors='coerce')
-dataset['END_DATE'] = pd.to_datetime(dataset['END_DATE'], errors='coerce')
+# -----------------------------------------------
+# ## data processing: replacing the null value with the NOT keyword
+# print(dataset['PURPOSE'].fillna("NOT", inplace=True))
 
-## changing the start_date to date and time 
-## converting the time into four different categories i.e., Morning, Afternoon, Evening, Night
+# ## changing start_date and end_date to the date_time format
+# dataset['START_DATE'] = pd.to_datetime(dataset['START_DATE'], errors='coerce')
+# dataset['END_DATE'] = pd.to_datetime(dataset['END_DATE'], errors='coerce')
 
-from datetime import datetime
+# ## changing the start_date to date and time 
+# ## converting the time into four different categories i.e., Morning, Afternoon, Evening, Night
 
-dataset['date'] = pd
+
+# dataset['date'] = pd
+
+# -------------------------------------
+# DATA VISUALISATION
+# -------------------------------------
+#1. In this, we will understand and compare all the columns in the dataset.
+
+## Checking the unique values in the dataset of the columns:
+
+obj = (dataset.dtypes == 'object')
+object_columns = list(obj[obj].index)
+
+unique_values = {}
+for col in object_columns:
+    unique_values[col] = dataset[col].unique().size
+print(unique_values)
